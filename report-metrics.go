467290efbbc0e9a4b750667793ec56cd7271a545
Vahid <vahidmostofi@outlook.com>
2020-12-24 08:20:17 -0700

    report-metrics parser done

--- report-metrics.go
+++ report-metrics.go
@@ -4,8 +4,11 @@ package main
 
 import (
 	"fmt"
-	"encoding/json"
+	"os"
 	"context"
+	"time"
+	"strconv"
+	"strings"
 	discovery "github.com/gkarthiks/k8s-discovery"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
@@ -14,23 +17,56 @@ var (
 	k8s *discovery.K8s
 )
 
+type Usage struct{
+	PodName string
+	CPU int64
+	Memory int64
+}
+
+var measurementName string
+
+func report(u Usage){
+	//format: https://docs.influxdata.com/influxdb/v2.0/reference/syntax/line-protocol/
+	fmt.Println(measurementName + ","+"podName=" + u.PodName + " " + "cpu="+strconv.FormatInt(u.CPU,10)+","+"memory="+strconv.FormatInt(u.Memory,10))
+}
+
 func main() {
 	k8s, _ = discovery.NewK8s()
-	namespace, _ := k8s.GetNamespace()
-	version, _ := k8s.GetVersion()
-	fmt.Printf("Specified Namespace: %s\n", namespace)
-	fmt.Printf("K8s version: %s\n", version)
-	podMetrics, err := k8s.MetricsClientSet.MetricsV1beta1().PodMetricses(namespace).List(context.Background(), metav1.ListOptions{})
-	if err != nil {
-		panic(err)
+	namespace := os.Getenv("METRICS_MONITOR_K8S_NAMESPACE")
+	interval := os.Getenv("METRICS_MONITOR_INTERVAL") // interval in seconds
+	if len(interval) == 0{
+		interval = "10"
 	}
-	for _, lm := range podMetrics.Items{
-		
-		b,_ := json.Marshal(lm)
-		fmt.Println(string(b))
-		for _, c := range lm.Containers{
-			fmt.Println("cpu",c.Usage.Cpu().ToDec())
+	intervalValue, err := strconv.Atoi(interval)
+	if err != nil{
+		panic(fmt.Sprintf("cant parse interval value: %s, %w", interval, err))
+	}
+	measurementName = os.Getenv("METRICS_MONITOR_MEASUREMENT_NAME")
+	if len(measurementName) == 0{
+		measurementName = "resource_usage"
+	}
+	// version, _ := k8s.GetVersion()
+	// fmt.Printf("Specified Namespace: %s\n", namespace)
+	// fmt.Printf("K8s version: %s\n", version)
+	for {
+		podMetrics, err := k8s.MetricsClientSet.MetricsV1beta1().PodMetricses(namespace).List(context.Background(), metav1.ListOptions{})
+		if err != nil {
+			panic(err)
 		}
-		break
+		for _, pdItem := range podMetrics.Items{
+			parts := strings.Split(pdItem.GetObjectMeta().GetSelfLink(), "/")
+			podName := parts[len(parts)-1]
+			u := Usage{
+				podName,
+				0, 
+				0,
+			}
+			for _, c := range pdItem.Containers{
+				u.CPU += c.Usage.Cpu().MilliValue()
+				u.Memory += c.Usage.Memory().MilliValue()
+			}
+			report(u)
+		}
+		time.Sleep(time.Duration(intervalValue) * time.Second)
 	}
 }
